44cd650d3ad7473e979376b6e110f327b87d0493
leshkoroman <leshkoroman@gmail.com>
2016-09-07 18:43:31 +0300

    site-start-16

---
+++ common/config/database.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+'class' => 'yii\db\Connection',
+ 'dsn' => 'mysql:host=localhost;dbname=merapoisk_crm',
+ 'username' => 'max',
+ 'password' => '123roman123max',
+ 'charset' => 'utf8',
+];
